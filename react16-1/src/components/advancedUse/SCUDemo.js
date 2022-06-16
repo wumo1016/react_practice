a84b04fee2003afa4e08c5b0534fda2284f7a4de
wumo1016 <760478279@qq.com>
2022-06-16 21:55:00 +0800

    feat(SCU)

--- react16-1/src/components/advancedUse/SCUDemo.js
+++ react16-1/src/components/advancedUse/SCUDemo.js
@@ -1,30 +1,40 @@
 import React from 'react'
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            count: 0
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0,
+      age: 0
     }
-    render() {
-        return <div>
-            <span>{this.state.count}</span>
-            <button onClick={this.onIncrease}>increase</button>
-        </div>
+  }
+  render() {
+    return (
+      <div>
+        <div>count: {this.state.count}</div>
+        <div>age: {this.state.age}</div>
+        <button onClick={this.onIncrease}>increase</button>
+        <button onClick={this.onIncrease1}>increase1</button>
+      </div>
+    )
+  }
+  onIncrease = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  onIncrease1 = () => {
+    this.setState({
+      age: this.state.age + 1
+    })
+  }
+  // 演示 shouldComponentUpdate 的基本使用
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.count !== this.state.count) {
+      return true // 可以渲染
     }
-    onIncrease = () => {
-        this.setState({
-            count: this.state.count + 1
-        })
-    }
-    // 演示 shouldComponentUpdate 的基本使用
-    shouldComponentUpdate(nextProps, nextState) {
-        if (nextState.count !== this.state.count) {
-            return true // 可以渲染
-        }
-        return false // 不重复渲染
-    }
+    return false // 不重复渲染
+  }
 }
 
 export default App
--- react16-1/src/components/advancedUse/index.js
+++ react16-1/src/components/advancedUse/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 // import UncontrolledDemo from './UncontrolledDemo'
 // import PortalsDemo from './PortalsDemo'
 // import ContextDemo from './ContextDemo'
-import LazyDemo from './LazyDemo'
+// import LazyDemo from './LazyDemo'
 // import SCUDemo from './SCUDemo'
-// import SCUDemo2 from './SCUDemo2'
+import SCUDemo2 from './SCUDemo2'
 // import PureComponentDemo from './PureComponentDemo'
 // import HOCDemo from './HOCDemo'
 // import RenderPropDemo from './RenderPropDemo'
@@ -19,9 +19,9 @@ class AdvancedUse extends React.Component {
         {/* <UncontrolledDemo /> */}
         {/* <PortalsDemo>Modal 内容</PortalsDemo> */}
         {/* <ContextDemo /> */}
-        <LazyDemo />
-        {/* <SCUDemo/> */}
-        {/* <SCUDemo2/> */}
+        {/* <LazyDemo /> */}
+        {/* <SCUDemo /> */}
+        <SCUDemo2 />
         {/* <PureComponentDemo/> */}
         {/* <HOCDemo a="100"/> */}
         {/* <RenderPropDemo a="200" /> */}
